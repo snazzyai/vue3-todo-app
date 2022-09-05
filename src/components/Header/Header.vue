66a04adde3302f4b1874e6c867a31cfec2adcf5e
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-09-05 09:18:47 +0200

    Updated Header with store

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useTaskStore } from '@/stores/task'
+
+const taskStore = useTaskStore()
+
+
+</script>
+
+<template>
+  <header>
+    <h4>Welcome, Stan </h4>
+    <p>You've got {{taskStore.getNumberOfTasks}} tasks coming up in the next days</p>
+</header>
+</template>
+
+<style scoped>
+
+</style>
