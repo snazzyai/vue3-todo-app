77a34c0a0d027f8d1292f93873d9682ebadcfe98
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-09-05 09:17:56 +0200

    changed import url

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
+import App from '@/App.vue'
 
 
 import './assets/main.css'
