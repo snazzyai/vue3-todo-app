d2d058a3cc9a1149375031e5ffcfb62c328344ee
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-09-05 21:21:38 +0200

    creeated alert component

---
+++ src/components/Alert/Alert.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps({
+    error: {
+        type: String
+    }
+})
+
+
+</script>
+
+<template>
+
+    <div class="alert alert-warning">{{ props.error }}</div>
+</template>
+
+<style scoped>
+</style>
