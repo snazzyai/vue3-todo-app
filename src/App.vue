c3a0aa54f63ff7b4a9b1cd2a94690d411a94e56c
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-09-05 09:17:11 +0200

    updated to use components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,17 @@
 <script setup lang="ts">
+import Header from '@/components/Header/Header.vue'
+import Todo from '@/components/Todo/Todo.vue'
+import TodoList from '@/components/TodoList/TodoList.vue'
 </script>
 
 <template>
-  <header>
-  </header>
-
+  <Header />
   <main>
+    <Todo />
+    <TodoList />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
