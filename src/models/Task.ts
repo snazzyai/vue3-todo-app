1d6d7f6d3ca3915c6f85cf00061abcd914e802c6
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-09-05 09:20:35 +0200

    created interface model

---
+++ src/models/Task.ts
@@ -0,0 +1,9 @@
+export type Category = Array<string>
+
+export interface Task {
+    id: string,
+    label: string,
+    description: string,
+    category: Category,
+    isCompleted?: Boolean 
+}
